bbe6741ed6f68b69c08affda20d0114f6caa385a
shanME <giorgie103188@gmail.com>
2024-09-09 18:53:24 +0400

    new post

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import classe from "./PostsList.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
 
 const PostsList = () => {
   return (
-    <ul className={classe.posts}>
-      <Post author="Giorgi" body="React.js is awesome" />
-      <Post author="Tiko" body="Hello World" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classe.posts}>
+        <Post author="Giorgi" body="React.js is awesome" />
+        <Post author="Tiko" body="Hello World" />
+      </ul>
+    </>
   );
 };
 
