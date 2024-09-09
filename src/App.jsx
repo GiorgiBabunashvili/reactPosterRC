a10421defaf35ca7b48315ed304c182ab34813c4
shanME <giorgie103188@gmail.com>
2024-09-09 18:43:31 +0400

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Post from "./components/Post";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <main>
+      <Post author="Giorgi" body="React.js is awesome" />
+      <Post author="Tiko" body="Hello World" />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import classes from "./Post.module.css";
 
-const Post = () => {
-  return <div></div>;
+const Post = (props) => {
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </div>
+  );
 };
 
 export default Post;
